a735c3674534d8b60ef72b54e8adb6dfb95b57a4
Troy Tomasch <troyrh6@gmail.com>
2024-08-15 09:07:03 -0400

    Fixing contact string

--- src/strings.js
+++ src/strings.js
@@ -11,6 +11,6 @@ const homePageString =
   "Welcome to Maker Wear, where craftsmanship meets protection. Our mission is to provide top-quality protective clothing tailored for creators like you. Discover our signature Maker Pants—one-size-fits-most overalls designed for comfort and durability, now available for pre-order. At Maker Wear, we understand the unique needs of makers and offer custom clothing solutions crafted by makers, for makers. Safeguard your work and your wardrobe with gear that stands up to the demands of your creative process.";
 
 const contactMeString =
-  "Interested in something from Maker Wear? Reach out and let us know what your interested in and what you want to use the product for. We will get back to you within 24 hours. \n You can contact me through my email, ";
+  "Interested in something from Maker Wear? Reach out and let us know what your interested in and what you want to use the product for. We will get back to you within 24 hours. \n You can reach us through our email, ";
 
 export { makerpantsFeature, homePageString, contactMeString };
